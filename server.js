b71be3aca85902b3cc49155f1c0cb6c30557ee1c
eric-lewis94 <mwangie27@gmail.com>
2023-04-10 18:17:58 +0300

    updates

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
+let mongodb_url = 'mongodb+srv://Newuser:Newuser@cluster0.x1xdpuo.mongodb.net/?retryWrites=true&w=majority';
+let dbName = 'cluster1';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
 });
